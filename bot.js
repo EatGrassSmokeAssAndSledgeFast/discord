a7bc77bc234adab96ea1008f515d6d60cfad3f21
EatGrassSmokeAssAndSledgeFast <42079513+EatGrassSmokeAssAndSledgeFast@users.noreply.github.com>
2018-08-04 04:02:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ bot.on("ready", () => {
           message.channel.send("Best Colour Ever **Made By Zack 2.0**");
           break;
       case "RiffRaff":
-          message.channel.send("My main goal is to blow up, then act like I dont know no body. - Riff Raff (donnu what year)");
+          message.channel.send("My main goal is to blow up, then act like I dont know no body. - Riff Raff **donnu what year** ");
           break;
            case "ban" :
 if (!message.guild.members.get("457813876407533569").hasPermission("BAN_MEMBERS")) return message.reply("I do not have permission to ban other members.")
