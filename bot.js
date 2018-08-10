d2ab9f3bb577626fedac65670f9451f67eba5930
EatGrassSmokeAssAndSledgeFast <42079513+EatGrassSmokeAssAndSledgeFast@users.noreply.github.com>
2018-08-10 17:30:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client()
 
-const TOKEN = "NDU3ODEzODc2NDA3NTMzNTY5.Dk8xPg.7PX6fxMO0qVucL1lcumxe0ZwwQQ"
+const TOKEN = "NDU3ODEzODc2NDA3NTMzNTY5.Dk8xPg.7PX6fxMO0qVucL1lcumxe0Zww"
 
 const PREFIX = "///"
 
